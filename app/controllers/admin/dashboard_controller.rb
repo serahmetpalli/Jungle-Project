b2161ad004fca88e2ce96a2dec346af12784826b
Serah Sirohi <serahsirohi@gmail.com>
2020-09-22 19:00:24 +0000

    added product and category count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['AUTH_USERNAME'].to_s, password: ENV['AUTH_PASSWORD'].to_s
   def show
+    @product_count= Product.count
+    @category_list= Product.group(:category_id).count
   end
 end
