266b880728e464927e191f313ba2cc8cbc4434c1
Serah Sirohi <serahsirohi@gmail.com>
2020-09-22 02:37:19 +0000

    added admin auth username and password

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  http_basic_authenticate_with name: ENV['USERNAME'].to_s, password: ENV['PASSWORD'].to_s
 
   def index
     @products = Product.all.order(created_at: :desc)
