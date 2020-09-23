3cbe16d541105d4649fddb903c06694fc9be138a
Serah Sirohi <serahsirohi@gmail.com>
2020-09-23 21:52:01 +0000

    feature/testing-home-page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  http_basic_authenticate_with name: ENV['AUTH_USERNAME'].to_s, password: ENV['AUTH_PASSWORD'].to_s 
+  # http_basic_authenticate_with name: ENV['AUTH_USERNAME'].to_s, password: ENV['AUTH_PASSWORD'].to_s 
 
   def show
     @category = Category.find(params[:id])
